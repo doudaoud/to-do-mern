16598aff946cc448f7c423e52033062c77e88582
doudaoud <boudaoudreda1@gmail.com>
2026-03-08 18:45:51 +0100

    fix(hero2): afficher correctement les icônes des cards en passant des composants JSX au lieu de strings

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import "./styles/card.css";
 export default function Card(props) {
   const { logo, title, discreption } = props;
   return (
     <>
       <div className="container_carte">
         <div className="icon_place">
-            
+            {logo}
+        </div>
+        <div className="text_place">
+          <h2>
+            {title}
+          </h2>
+          <p>
+            {discreption}
+          </p>
         </div>
       </div>
     </>
--- client/src/components/Hero2.jsx
+++ client/src/components/Hero2.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./styles/hero2.css";
-
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import Card from "./Card";
 export default function Hero2() {
+  const informations = [
+    {
+      logo: <CheckCircleOutlineIcon/>,
+      title: "Listes intelligentes",
+      discreption:
+        "Créez et organisez vos tâches en listes catégorisées. Marquez les tâches comme complétées en un clic.",
+    },
+    {
+      logo: <AccessTimeIcon/>,
+      title: "`Planification temporelle",
+      discreption:
+        "Définissez des dates d'échéance et des rappels pour ne jamais oublier une tâche importante.",
+    },
+    {
+      logo: <NotificationsNoneIcon/>,
+      title: "Notifications en temps réel",
+      discreption:
+        "Recevez des notifications instantanées pour rester à jour avec vos tâches prioritaires.",
+    },
+  ];
   return (
     <>
       <div className="hh">
@@ -13,8 +36,19 @@ export default function Hero2() {
             de tâches.
           </p>
         </div>
-        <div className="cards">
-            
+        <div 
+        className="cards">
+          {informations.map((info, index) => {
+            return (
+              <div key={index}>
+                <Card
+                  logo={info.logo}
+                  title={info.title}
+                  discreption={info.discreption}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
