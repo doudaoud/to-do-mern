05745be5f37c60748e97bac20180a2306a6de3c1
doudaoud <boudaoudreda1@gmail.com>
2026-03-12 18:26:12 +0100

    feat: Ajoute une section d'informations avec des cartes descriptives
    Ce commit ajoute une nouvelle section au composant Hero2 pour présenter des informations clés aux utilisateurs. Les modifications suivantes ont été apportées :
    
    - Ajout d'un nouveau composant Card pour afficher les informations avec un logo, un titre et une description.
    - Création d'un tableau d'informations dans Hero2 contenant les données pour chaque carte.
    - Mise en œuvre du rendu dynamique des cartes en utilisant la méthode `map()` sur le tableau d'informations.
    - Ajout de styles CSS pour la mise en page et l'apparence des cartes et de la section Hero2.
    - Ajout de l'image de statistique dans la page d'accueil.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,32 @@ import Navbar from "../components/navbar";
 import Hero from "../components/Hero";
 import { Link, Routes, Route } from "react-router-dom";
 import Hero2 from "../components/Hero2";
+import img from "../images/graphe.webp";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
       <Hero2 />
+      <div
+        className="inage-stat "
+        style={{
+          width: "90%",
+          margin: "auto",
+          height: "500px",
+          marginTop:200
+        }}
+      >
+        <img
+          src={img}
+          style={{
+            width: "95%",
+            margin: "auto",
+            height: "700px",
+            marginLeft:"33px"
+          }}
+        />
+      </div>
     </>
   );
 }
