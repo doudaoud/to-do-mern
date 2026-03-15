d69c352422b1f92abe2adb9cd882c5f3ce4c77a3
doudaoud <boudaoudreda1@gmail.com>
2026-03-15 17:53:15 +0100

    `Refactored Hero3 component and styles`

--- client/src/components/Hero3.jsx
+++ client/src/components/Hero3.jsx
@@ -5,18 +5,19 @@ import img from "../images/img_hero_3.webp";
 export default function Hero3() {
   return (
     <>
-      
       <div className="hero3">
         {/* TODO  regler le css */}
         <div className="hero3_container">
-          <img src={img} style={{
-            width: 584,
-            height: 584,
-            borderRadius: 10,
-            boxShadow: "0px 2px 10px rgba(0,0,0,0.3)",
-            marginLeft:99
-
-          }} />
+          <img
+            src={img}
+            style={{
+              width: 584,
+              height: 584,
+              borderRadius: 10,
+              boxShadow: "0px 2px 10px rgba(0,0,0,0.3)",
+              marginLeft: 99,
+            }}
+          />
         </div>
         <div className="titles_hero3">
           <h2>Pourquoi TodoApp ?</h2>
@@ -24,10 +25,28 @@ export default function Hero3() {
             Conçue avec simplicité et efficacité en tête, TodoApp vous aide à
             rester productif sans surcharge.
           </p>
-          <div className="icons">
-            <div></div>
-            <div></div>
-            <div></div>
+          <div className="part2">
+            <div className="icon_title">
+              <Zap className="icon" />
+              <div className="title_icon">
+                <h4>Ultra rapide</h4>
+                <p>Interface fluide et réactive</p>
+              </div>
+            </div>
+            <div className="icon_title">
+              <CheckCircle2 className="icon" />
+              <div className="title_icon">
+                <h4>Facile à utiliser</h4>
+                <p>Pas de courbe d'apprentissage</p>
+              </div>
+            </div>
+            <div className="icon_title">
+              <Clock className="icon" />
+              <div className="title_icon">
+                <h4>Synchronisation instantanée</h4>
+                <p>Accédez à vos tâches partout</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
